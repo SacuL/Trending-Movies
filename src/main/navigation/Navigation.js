7694ee1a2b84148d8e0e59311651d3a79f0754c6
Lucas Cerqueira <lucas44r@gmail.com>
2019-12-24 12:01:39 -0300

    Range slider on navigation

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -1,20 +1,55 @@
 import React from "react";
 import "./Navigation.css";
 import Selection from "./Selection";
+import Slider from "./Slider";
 
 class Navigation extends React.Component {
   state = {
-    genre: "comedy"
+    genre: "comedy",
+    year: {
+      label: "year",
+      min: 1990,
+      max: 2017,
+      step: 1,
+      value: { min: 2000, max: 2017 }
+    },
+    rating: {
+      label: "rating",
+      min: 0,
+      max: 10,
+      step: 1,
+      value: { min: 8, max: 10 }
+    },
+    runtime: {
+      label: "runtime",
+      min: 0,
+      max: 300,
+      step: 15,
+      value: { min: 60, max: 120 }
+    }
   };
 
   setGenre = value => {
     this.setState({ genre: value });
   };
 
+  onChange = data => {
+    this.setState({
+      [data.type]: {
+        ...this.state[data.type],
+        value: data.value
+      }
+    });
+  };
+
   render() {
     return (
       <section className="navigation">
         <Selection genre={this.state.genre} onGenreChange={this.setGenre} />
+
+        <Slider data={this.state.year} onChange={this.onChange} />
+        <Slider data={this.state.rating} onChange={this.onChange} />
+        <Slider data={this.state.runtime} onChange={this.onChange} />
       </section>
     );
   }
