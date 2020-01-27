1ffda7312dbdf9ac04249de1f6ec79d3dbedc18b
Lucas Cerqueira <lucas44r@gmail.com>
2020-01-27 19:22:25 -0300

    Build & Deploy

---
+++ docs/precache-manifest.c880696cd16b55c28c048907d245257a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e875c0aae22acb5f1375a35397b4b2aa",
+    "url": "/Trending-Movies/index.html"
+  },
+  {
+    "revision": "4960f351c41b724f0ca8",
+    "url": "/Trending-Movies/static/css/2.82f03f93.chunk.css"
+  },
+  {
+    "revision": "00f7fce0ef3b7adc954b",
+    "url": "/Trending-Movies/static/css/main.67779942.chunk.css"
+  },
+  {
+    "revision": "4960f351c41b724f0ca8",
+    "url": "/Trending-Movies/static/js/2.9856bf3e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Trending-Movies/static/js/2.9856bf3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00f7fce0ef3b7adc954b",
+    "url": "/Trending-Movies/static/js/main.a7a83621.chunk.js"
+  },
+  {
+    "revision": "d3e661257e0c9b08c817",
+    "url": "/Trending-Movies/static/js/runtime-main.9465eb54.js"
+  }
+]);
